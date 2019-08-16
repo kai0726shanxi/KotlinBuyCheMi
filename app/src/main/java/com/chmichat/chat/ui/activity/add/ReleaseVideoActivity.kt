4e7df4f04ecc449966d5aaae206254b87cac7be6
20342 <https://gitee.com/gkdx/MyTest.git>
2019-08-16 16:17:07 +0800

    删除

---
+++ app/src/main/java/com/chmichat/chat/ui/activity/add/ReleaseVideoActivity.kt
@@ -0,0 +1,22 @@
+package com.chmichat.chat.ui.activity.add
+
+import com.chmichat.chat.base.BaseActivity
+
+/**
+ * @Author 20342
+ * @Date 2019/8/16 15:50
+ */
+class ReleaseVideoActivity:BaseActivity() {
+    override fun layoutId(): Int {
+      return 0
+    }
+
+    override fun initData() {
+    }
+
+    override fun initView() {
+    }
+
+    override fun start() {
+    }
+}
